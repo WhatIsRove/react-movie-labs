439f2c4bcdb0c951320e669686e5da8c28424e68
WhatIsRove <whatisbuff@gmail.com>
2024-10-27 17:56:59 +0000

    Add to favourties logic ompleted; dummy favourites page.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -13,12 +13,33 @@ import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid2";
 import img from '../../images/film-poster-placeholder.png';
 import { Link } from "react-router-dom";
+import Avatar from '@mui/material/Avatar';
 
 export default function MovieCard(props) {
     const movie = props.movie;
+
+    const handleAddToFavourite = (e) => {
+        e.preventDefault();
+        props.selectFavourite(movie.id);
+    };
+
     return (
         <Card>
-            <CardHeader title={movie.title} sx={{ textWrap: "nowrap" }} />
+            <CardHeader
+                avatar={
+                    movie.favorite ? (
+                        <Avatar sx={{ backgroundColor: 'red' }}>
+                            <FavoriteIcon />
+                        </Avatar>
+                    ) : null
+                }
+                title={
+                    <Typography variant="h5" component="p">
+                        {movie.title}{" "}
+                    </Typography>
+                }
+            />
+
             <CardMedia
                 sx={{ height: 500 }}
                 image={
@@ -44,7 +65,7 @@ export default function MovieCard(props) {
                 </Grid>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favourites" onclick={null}>
+                <IconButton aria-label="add to favourites" onclick={handleAddToFavourite}>
                     <FavoriteIcon color="primary" fontSize="large" />
                 </IconButton>
                 <Link to={`/movies/${movie.id}`}>
--- movies/src/index.js
+++ movies/src/index.js
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
