4f797ffdb67d963df15d8d13249ffeedd8901874
WhatIsRove <whatisbuff@gmail.com>
2024-11-04 15:48:02 +0000

    Card icons - make action icons on movie card configurable.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -16,7 +16,7 @@ import { Link } from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
 import { MoviesContext } from "../../contexts/moviesContext";
 
-export default function MovieCard({movie}) {
+export default function MovieCard({movie, action}) {
     const { favourites, addToFavourites } = useContext(MoviesContext);
 
     if (favourites.find((id) => id === movie.id)) {
@@ -72,14 +72,15 @@ export default function MovieCard({movie}) {
                 </Grid>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favourites" onClick={handleAddToFavourite}>
-                    <FavouriteIcon color="primary" fontSize="large" />
-                </IconButton>
+
+                {action(movie)}
+
                 <Link to={`/movies/${movie.id}`}>
                     <Button variant="outlined" size="medium" color="primary">
                         More Info ...
                     </Button>
                 </Link>
+                
             </CardActions>
         </Card>
     );
--- movies/src/components/movieList/index.js
+++ movies/src/components/movieList/index.js
@@ -5,7 +5,7 @@ import Grid from "@mui/material/Grid2";
 const MovieList = (props) => {
     let movieCards = props.movies.map((m) => (
         <Grid key={m.id} size={{xs: 12, sm: 6, md: 6, lg: 8, xl: 3}} sx={{padding: "20px"}}>
-            <Movie key={m.id} movie={m} selectFavourite={props.selectFavourite} />
+            <Movie key={m.id} movie={m} action={props.action} />
         </Grid>
     ));
     return movieCards;
