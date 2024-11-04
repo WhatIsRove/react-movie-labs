8733ddcdc24089c90135d901aa815028b53e982d
WhatIsRove <whatisbuff@gmail.com>
2024-11-04 16:20:59 +0000

    New button for upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -3,7 +3,7 @@ import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api"
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner"
-import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+import AddToPlaylistIcon from "../components/cardIcons/addToPlaylist";
 
 const UpcomingMoviesPage = (props) => {
 
@@ -19,17 +19,16 @@ const UpcomingMoviesPage = (props) => {
 
     const movies = data.results;
 
-    const favourites = movies.filter(m => m.favourite)
-    localStorage.setItem('favourites', JSON.stringify(favourites))
+    //const favourites = movies.filter(m => m.favourite)
     
-    const addToFavourites = (movieId) => true;
+    //const addToFavourites = (movieId) => true;
 
     return (
         <PageTemplate 
             title='Upcoming Movies'
             movies={movies}
             action={(movie) => {
-                return <AddToFavouritesIcon movie={movie} />
+                return <AddToPlaylistIcon movie={movie} />
             }}
         />
     );
