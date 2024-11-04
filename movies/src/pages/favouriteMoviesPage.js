887538b980bbbe7fb4965985dced1faec9f2b090
WhatIsRove <whatisbuff@gmail.com>
2024-11-04 13:49:42 +0000

    Refactor: Favourite movie list now stored in a context.

--- movies/src/pages/favouriteMoviesPage.js
+++ movies/src/pages/favouriteMoviesPage.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import PageTemplate from "../components/templateMovieListPage"
-
+import { MoviesContext } from "../contexts/moviesContext";
+import { useQueries } from "react-query";
+import { getMovie } from "../api/tmdb-api";
+import Spinner from "../components/spinner";
 
 const FavouriteMoviesPage = () => {
+    const {favourites: movieIds } = useContext(MoviesContext)
+
+    const favouriteMovieQueries = useQueries(
+        movieIds.map((movieId) => {
+            return {
+                queryKey: ["movie", {id : movieId}],
+                queryFn: getMovie
+            }
+        })
+    )
+
+    const isLoading = favouriteMovieQueries.find((m) => m.isLoading === true)
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
+    const movies = favouriteMovieQueries.map((q) => {
+        q.data.genre_ids = q.data.genres.map(g => g.id)
+        return q.data
+    })
+
     const toDo = () => true;
-    const movies = JSON.parse(localStorage.getItem("favourites"));
 
     return (
         <PageTemplate 
